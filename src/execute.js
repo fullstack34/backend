2cf555ebfe36fdacbac866c0fcd406fc991e7236
Kelvys Moura <kelvysmoura2@gmail.com>
2024-09-28 11:59:32 -0300

    Usando throw e relacionamento 1:N entre user e posts

--- src/execute.js
+++ src/execute.js
@@ -1,13 +1,57 @@
 const UserModel = require('./models/UserModel');
+const PostsModel = require('./models/PostsModel');
+const { Op } = require('sequelize');
 
-/*UserModel.create({
-  firstname: 'Raimundo',
-  surname: 'da Silva',
-  username: 'raimundo',
-  email: 'raimundo@gmail.com',
-  password: '1234'
-}).then(resultado => {
-  console.log(resultado);
-});*/
+PostsModel.belongsTo(UserModel, {foreignKey: 'user_id'});
 
-UserModel.findAll().then(resultado => console.log(resultado));
+async function execute() {
+
+  await PostsModel.create({
+    user_id: 1,
+    title: "Aprendendo CSS",
+    content: "Lorem ipsum dolor sit amet, consectetur adip"
+  });
+
+  let post = await PostsModel.findOne({
+    include: UserModel
+  });
+
+  console.log(post.UserModel.email);
+
+
+
+
+
+
+
+
+
+
+
+
+    /*await UserModel.create({
+      firstname: 'Joaquim',
+      surname: 'da Silva',
+      username: 'joaquim' + Math.random(),
+      email: 'joaquim@gmail.com.' + Math.random(),
+      password: '1234'
+    });
+
+    let users = await UserModel.findAll({
+      where: {
+        [Op.or]: [{username: 'joaquim'}, {id: 1}]
+      }
+    });
+
+    let result = await UserModel.destroy({
+      where: {
+        id: {
+          [Op.gte]: 15
+        }
+      }
+    });
+
+    console.log(result);*/
+}
+
+execute();
