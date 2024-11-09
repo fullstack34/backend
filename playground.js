41e0af45d94c60a8d34cfaf1bea028dbb522ac33
kelvysmoura <kelvysmoura2@gmail.com>
2024-11-09 12:06:44 -0300

    Salvando imagem base64 e salvando no banco

--- playground.js
+++ playground.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
 const img = `/9j/4QDeRXhpZgAASUkqAAgAAAAGABIBAwABAAAAAQAAABoBBQABAAAAVgAAABsBBQABAAAAXgAAACgBAwABAAAAAgAAABMCAwABAAAAAQAAAGmHBAABAAAAZgAAAAAAAABIAAAAAQAAAEgAAAABAAAABwAAkAcABAAAADAyMTABkQcABAAAAAECAwCGkgcAFgAAAMAAAAAAoAcABAAAADAxMDABoAMAAQAAAP//AAACoAQAAQAAAAoAAAADoAQAAQAAAAoAAAAAAAAAQVNDSUkAAABQaWNzdW0gSUQ6IDM0MP/bAEMACAYGBwYFCAcHBwkJCAoMFA0MCwsMGRITDxQdGh8eHRocHCAkLicgIiwjHBwoNyksMDE0NDQfJzk9ODI8LjM0Mv/bAEMBCQkJDAsMGA0NGDIhHCEyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMv/CABEIAAoACgMBIgACEQEDEQH/xAAXAAADAQAAAAAAAAAAAAAAAAAAAQQF/8QAFQEBAQAAAAAAAAAAAAAAAAAAAQL/2gAMAwEAAhADEAAAAcWUcn//xAAZEAEAAgMAAAAAAAAAAAAAAAACAQMQERL/2gAIAQEAAQUCglHm3dOP/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAgBAwEBPwF//8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAgBAgEBPwF//8QAGhAAAgIDAAAAAAAAAAAAAAAAAAIBIRAxcf/aAAgBAQAGPwKqY1Ivcf/EABoQAQADAAMAAAAAAAAAAAAAAAEAETEhofD/2gAIAQEAAT8hPt9A9sUXXFzFW9n/2gAMAwEAAgADAAAAELP/xAAVEQEBAAAAAAAAAAAAAAAAAAAAAf/aAAgBAwEBPxCv/8QAFxEBAAMAAAAAAAAAAAAAAAAAAQARIf/aAAgBAgEBPxC0yf/EABwQAQEAAgIDAAAAAAAAAAAAAAERACExUXGBsf/aAAgBAQABPxAxWiXZr416xwBFUKh4xI1hIXi84NApe8//2Q==`;
 let url = 'https://picsum.photos/200/300';
+const path = require("path");
+
+const mimeTypeMap = {
+    "image/png": 'png',
+    "image/jpg": 'jpg',
+    "image/jpeg": 'jpeg',
+    "image/webp": 'webp',
+    "image/jfif": "jfif"
+};
 
 async function execute() {
-    let decode = atob(img);
-    fs.writeFileSync('teste.png', decode, {
-        encoding: 'binary',
+    let response = await fetch(url);
+    let mimeType = response.headers.get('content-type');
+    let extension = mimeTypeMap[mimeType];
+    let buffer = await response.arrayBuffer();
+    buffer = Buffer.from(buffer, 'binary');
+    console.log(buffer)
+    fs.writeFileSync(`teste.${extension}`, buffer, {
+        encoding: 'binary'
     });
 }
 
--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,17 +1,60 @@
 const fs = require('fs');
+const path = require('path');
+const PostsModel = require('../models/PostsModel');
+
+const mimeTypeMap = {
+    "image/png": 'png',
+    "image/jpg": 'jpg',
+    "image/jpeg": 'jpeg',
+    "image/webp": 'webp',
+    "image/jfif": "jfif"
+};
 
 const CreatePost = async (request, response) => {
+    let {type, mime, content} = request.body.image;
+    let {user_id, title} = request.body;
+
     try {
-        let base64 = request.body.image.content;
-        base64 = atob(base64);
-        fs.writeFileSync('teste.jpg', base64, {
-            encoding: 'binary'
+        let directory = path.resolve('static/images');
+        let filename = Math.random().toString(16).slice(2);
+
+        if(!fs.existsSync(directory)) {
+            fs.mkdirSync(directory, {recursive: true});
+        }
+
+        if(!content || !type) {
+            throw new Error("Body inválido");
+        }
+
+        if(type === 'base64') {
+            if(!mime) {
+                throw new Error("mime é obrigatorio para o type base64");
+            }
+            
+            let extension = mimeTypeMap[mime];
+            if(!extension) {
+                throw new Error("Mime inválido");
+            }
+
+            filename = `${filename}.${extension}`;
+            fs.writeFileSync(`${directory}/${filename}`, atob(content), {
+                encoding: 'binary'
+            });
+        }
+        
+        // upload por URL
+
+        await PostsModel.create({
+            user_id,
+            title,
+            image: filename
         });
+
         return response.json("Post criado");
     } catch (error) {
         console.log(error.message);
         response.status(400);
-        return response.json({message: "Erro ao criar post"});
+        return response.json({message: error.message});
     }
 }
 
