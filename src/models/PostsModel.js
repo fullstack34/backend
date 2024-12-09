1f15e9b6ebfd9330275b21b957c2304d9274088e
pedro0Ag <mesquitapedro752@gmail.com>
2024-12-09 18:50:01 -0300

    criando coluna virtual dete  para fornecer a hora nos padrões do Brasil

--- src/models/PostsModel.js
+++ src/models/PostsModel.js
@@ -28,6 +28,18 @@ const PostsModel = connection.define('PostsModel', {
     get() {
       return `http://localhost:3000/public/images/${this.getDataValue('image')}`;
     }
+  },
+  date: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      
+const createdAt = new Date(this.getDataValue('createdAt'));
+const day = String(createdAt.getDate()).padStart(2, '0');
+const month = String(createdAt.getMonth() + 1).padStart(2, '0');
+const year = createdAt.getFullYear();
+
+return `${day}/${month}/${year}`;
+    }
   }
 }, {
   tableName: 'posts'
